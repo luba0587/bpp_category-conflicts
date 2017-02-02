c4c0658fb826229e9eaf735f9e8b062573a94708
luba0587 <cavalcanti.lb@gmail.com>
2017-02-02 10:25:15 -0200

    Update HeaderBPP.h

--- HeaderBPP.h
+++ HeaderBPP.h
@@ -8,11 +8,14 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-//#include "mersenne.cpp"
 #include "randomc.h"
+#include <chrono>
+#include <Windows.h>
 
 
-#define M_GRANDE 1000000
+//#define M_GRANDE 1000000
+#define INST_AUGMENTED
+
 
 #ifdef M_GRANDE
 #include "gurobi_c++.h"
@@ -22,111 +25,143 @@
 using namespace std;
 
 
-/*************************************CLASSE CATEGORIES E SEUS M…TODOS **************************************/
+/*************************************CLASSE para medir tempo de processamento **************************************/
+class Timer
+{
+public:
+	Timer() : beg_(clock_::now()) {}
+	void reset() { beg_ = clock_::now(); }
+	double elapsed() const {
+		return std::chrono::duration_cast<second_>
+			(clock_::now() - beg_).count();
+	}
+
+private:
+	typedef std::chrono::high_resolution_clock clock_;
+	typedef std::chrono::duration<double, std::ratio<1> > second_;
+	std::chrono::time_point<clock_> beg_;
+};
+
+/*************************************CLASSE CATEGORIES E SEUS M‚Ä¶TODOS **************************************/
 class Categories {
 
 public:
 
-	int index; 		//Ìndice da categoria, de 0 a nCategorias-1
+	int index; 		//√åndice da categoria, de 0 a nCategorias-1
 	int compDegree;	//grau de compatibilidade da categoria
 
-	//mÈtodo que inicializa Categoria usando seu Ìndice e seu grau de compatibilidade
+	//m√àtodo que inicializa Categoria usando seu √åndice e seu grau de compatibilidade
 	Categories(int k, int degree)
 		: index(k), compDegree(degree)
 	{}
 
 };
 
-/*************************************ESTRUTURAS DE DADOS e FUN«’ES GLOBAIS****************************************/
+/*************************************ESTRUTURAS DE DADOS e FUN¬´‚ÄôES GLOBAIS****************************************/
 
-//matriz de compatibilidades, m por m, em que 0 indica incompatibilidade entre a categoria da linha e da coluna em quest„o (estrutura declarada no mesmo arquivo do main)
+//matriz de compatibilidades, m por m, em que 0 indica incompatibilidade entre a categoria da linha e da coluna em quest‚Äûo (estrutura declarada no mesmo arquivo do main)
 extern vector< vector<int> > compatibilityMatrix;
 
 //vetor de categorias do problema (estrutura declarada no mesmo arquivo do main)
 extern vector<Categories> allCategories;
 
-//funÁ„o que imprime uma matriz (vetor de vetores) na tela (definido em cInstances)
+//fator de capacidade do problema
+extern double INST_NEW;
+
+//fun√Å‚Äûo que imprime uma matriz (vetor de vetores) na tela (definido em cInstances)
 extern void printMatrix(vector< vector<int> > matrix);
 
-/*************************************CLASSE INSTANCIAS E SEUS M…TODOS ****************************************/
+/*************************************CLASSE INSTANCIAS E SEUS M‚Ä¶TODOS ****************************************/
 class Instances {
 
 private:
 
 	string inputFile;		//nome do arquivo com dados de entrada
 	int binCapacity;		//capacidade do bin
-	int mCategories;		//n˙mero de categorias da inst‚ncia
-	int nElements;			//n˙mero de elementos da inst‚ncia
-
+	int mCategories;		//nÀômero de categorias da inst‚Äöncia
+	int nElements;			//nÀômero de elementos da inst‚Äöncia
+	int lowerBound;			//lower bound, dado pela solu√Å‚Äûo do problema relaxado (sem compatiblidades e com lineariza√Å‚Äûo de xij)
+	int bestLB;				//melhor lower bound conhecido - solu√Å‚Äûo √õtima ou resultado de relaxa√Å‚Äûo				
 
 	/*************************************ESTRUTURAS DE DADOS ****************************************/
 
 	//vetor de objetos que devem ser inseridos nos bins, ordenados por categoria
-	//obs: a posiÁ„o È o Ìndice do objeto e o valor armazenado indica o peso
+	//obs: a posi√Å‚Äûo √à o √åndice do objeto e o valor armazenado indica o peso
 	vector<int> allElements;
 
 	//vetor que associa loja a cada objeto de allElements
-	//obs: a posiÁ„o È o Ìndice do objeto e o valor armazenado È o Ìndice da loja
+	//obs: a posi√Å‚Äûo √à o √åndice do objeto e o valor armazenado √à o √åndice da loja
 	vector<int> storeOfElement;
 
-	//vetor com a posiÁ„o do primeiro elemento de cada categoria no vetor que guarda todos os objetos 
-	//obs: a posiÁ„o indica categoria e o valor indica posiÁ„o no vetor allElements
+	//vetor com a posi√Å‚Äûo do primeiro elemento de cada categoria no vetor que guarda todos os objetos 
+	//obs: a posi√Å‚Äûo indica categoria e o valor indica posi√Å‚Äûo no vetor allElements
 	vector<int> firstObjectPosition;
 
+	//m√àtodo que l√ç e grava a capacidade dos bins e os dados referentes aos objetos, armazenados nas estruturas de dados 
+	void load();
+
+	//m√àtodo que resolve problema relaxado
+	void solveRelaxedProblem();
 
 public:
 
-	//construtor que cria inst‚ncia usando o nome do arquivo de entrada
+	//construtor que cria inst‚Äöncia usando o nome do arquivo de entrada
 	Instances(string str, int m);
 
-	//mÈtodo que lÍ e grava a capacidade dos bins e os dados referentes aos objetos, armazenados nas estruturas de dados 
-	void load();
-
-	//mÈtodo que retorna o vetor com todos os objetos
+	//m√àtodo que retorna o vetor com todos os objetos
 	vector<int> getElements() { return allElements; }
 
-	//mÈtodo que retorna o peso do objeto j
+	//m√àtodo que retorna o peso do objeto j
 	int getElementWeight(int j) { return allElements.at(j); }
 
-	//mÈtodo que retorna o vetor com primeira posiÁ„o de cada categoria
+	//m√àtodo que retorna o vetor com primeira posi√Å‚Äûo de cada categoria
 	vector<int> getCategoryPosition() { return firstObjectPosition; }
 	
-	//mÈtodo que retorna informaÁ„o do nome da inst‚ncia
+	//m√àtodo que retorna informa√Å‚Äûo do nome da inst‚Äöncia
 	string getName() { return inputFile; }
 
-	//mÈtodo que retorna informaÁ„o de capacidade dos bins da inst‚ncia
+	//m√àtodo que retorna informa√Å‚Äûo de capacidade dos bins da inst‚Äöncia
 	int getBinCapacity() { return binCapacity; }
 
-	//mÈtodo que retorna informaÁ„o de quantidade de categorias
+	//m√àtodo que retorna informa√Å‚Äûo de quantidade de categorias
 	int getM() { return mCategories; }
 	
-	//mÈtodo que retorna informaÁ„o de quantidade de elementos da inst‚ncia
+	//m√àtodo que retorna informa√Å‚Äûo de quantidade de elementos da inst‚Äöncia
 	int getN() { return nElements; }
 
+	//m√àtodo que retorna o lower bound do problema relaxado
+	int getRelaxedLB() { return lowerBound; }
+
+	//m√àtodo que retorna o best lower bound do problema
+	int getBestLB() { return bestLB; }
+
+	//m√àtodo que infere melhor LB do problema
+	void setBestLB(int z) { bestLB = z; }
+
 #ifdef M_GRANDE
-	//mÈtodo que acha soluÁ„o Ûtima do problema usando o Gurobi
-	void findLB();
+	//m√àtodo que acha solu√Å‚Äûo √õtima do problema usando o Gurobi e retorna o nÀômero de bins
+	vector<int> findOptimum();
 #endif
 
 };
 
 
-/*************************************CLASSE ELEMENTS E SEUS M…TODOS ****************************************/
+/*************************************CLASSE ELEMENTS E SEUS M‚Ä¶TODOS ****************************************/
 class Elements {
 
 public:
 
-	int index;		//Ìndice do elemento, de 0 a n-1
+	int index;		//√åndice do elemento, de 0 a n-1
 	int weight;		//peso do elemento
-	int category;	//Ìndice da categoria do elemento, de 0 a m-1
+	int category;	//√åndice da categoria do elemento, de 0 a m-1
 
-	//construtor: inicializa objeto usando seus Ìndice, peso e categoria (todos inteiros)
+	//construtor: inicializa objeto usando seus √åndice, peso e categoria (todos inteiros)
 	Elements(int j, int p, int k);
 
 };
 
 
-/*************************************CLASSE BINS E SEUS M…TODOS ****************************************/
+/*************************************CLASSE BINS E SEUS M‚Ä¶TODOS ****************************************/
 class Bins {
 
 public:
@@ -135,107 +170,117 @@ public:
 	int idleCapacity;			//capacidade ociosa do bin
 
 	vector<Elements> elements;	//vetor com elementos do bin
-	vector<int> catOnBin;		//vetor de M elementos, que indica o n˙mero de objetos da categoria k no bin
+	vector<int> catOnBin;		//vetor de M elementos, que indica o nÀômero de objetos da categoria k no bin
 
 	//construtor do bin: dada instancia e elementos, calcula idleCapacity e altera vetor de elementos e de categorias no bin
 	Bins(Instances &inst, Elements j);
 
-	//mÈtodo que verifica se elemento pode ser adicionado ao bin
+	//m√àtodo que verifica se elemento pode ser adicionado ao bin
 	bool binFit(Elements j);
 
-	//mÈtodo que adiciona elemento a um bin existente
+	//m√àtodo que adiciona elemento a um bin existente
 	void addElement(Elements j);
 
-	//mÈtodo que remove elemento de um bin usando o prÛprio elemento a ser removido como argumento
+	//m√àtodo que remove elemento de um bin usando o pr√õprio elemento a ser removido como argumento
 	void removeElement(Elements j);
 
-	//mÈtodo que remove elemento de um bin usando sua posiÁ„o no vetor de elementos
+	//m√àtodo que remove elemento de um bin usando sua posi√Å‚Äûo no vetor de elementos
 	void removeItemByPosition(int position);
 
 };
 
 
-/*************************************CLASSE SOLUTION E SEUS M…TODOS ****************************************/
+/*************************************CLASSE SOLUTION E SEUS M‚Ä¶TODOS ****************************************/
 class Solution {
 
 private:
 
-	string instanceName;			//nome do arquivo de entrada da inst‚ncia
-	vector<int> codifiedSolution;	//soluÁ„o codificada
-	int numberOfBins;				//n˙mero de bins utilizados	
-	double fitnessValue;			//valor na funÁ„o fitness
-	
+	string instanceName;			//nome do arquivo de entrada da inst‚Äöncia
+	vector<int> codifiedSolution;	//solu√Å‚Äûo codificada
+	int numberOfBins;				//nÀômero de bins utilizados	
+	float fitnessValue;				//valor na fun√Å‚Äûo fitness
+	float processingTime;			//tempo transcorrido para alcan√Åar essa solu√Å‚Äûo
 
 public:
 
 	vector<Bins> binsVector;		//vetor de bins - acessa primeiro bin, depois seu vetor de elementos
-	vector< vector<int> > solution;	//soluÁ„o em formato matricial: cada linha È um bin
+	vector< vector<int> > solution;	//solu√Å‚Äûo em formato matricial: cada linha √à um bin
 							
-	//construtor 1: dado nome da inst‚ncia e soluÁ„o em vetor ˙nico, cria matrix de soluÁ„o  e calcula o n˙mero de bins usado
+	//construtor 1: dado nome da inst‚Äöncia e solu√Å‚Äûo em vetor Àônico, cria matrix de solu√Å‚Äûo  e calcula o nÀômero de bins usado
 	Solution(string inst, vector<int> solutionCode);
 
-	//construtor 2: dado nome da inst‚ncia e vetor de bins, calcula fitness e n˙mero de bins, cria soluÁ„o codificada e matriz solution
+	//construtor 2: dado nome da inst‚Äöncia e vetor de bins, calcula fitness e nÀômero de bins, cria solu√Å‚Äûo codificada e matriz solution
 	Solution(string inst, vector<Bins> bins);
 
-	//mÈtodo que recalcula fitnessValue de uma soluÁ„o
+	//m√àtodo que recalcula fitnessValue de uma solu√Å‚Äûo
 	void calculateFitness();
 	
-	//mÈtodo que atualiza matrz solution de uma soluÁ„o
+	//m√àtodo que atualiza matrz solution de uma solu√Å‚Äûo
 	void updateSolution();
 
-	//mÈtodo que retorna o nome da inst‚ncia de uma soluÁ„o
+	//m√àtodo que retorna o nome da inst‚Äöncia de uma solu√Å‚Äûo
 	string getInstName() { return instanceName; }
 
-	//mÈtodo que retorna n˙mero de bins da soluÁ„o
+	//m√àtodo que retorna nÀômero de bins da solu√Å‚Äûo
 	int getNBins() { return numberOfBins; }
 
-	//mÈtodo que retorna o fitness de uma soluÁ„o
+	//m√àtodo que retorna o fitness de uma solu√Å‚Äûo
 	double getFitnessValue() { return fitnessValue; }
 
-	//mÈtodo que redefine n˙mero de bins de uma soluÁ„o
-	void resetNumberBins(int n) { numberOfBins = n; }
+	//m√àtodo que grava o tempo de processamento
+	void setTime(float t) { processingTime = t; }
 
+	//m√àtodo que retorna o tempo de rpocessamento de uma solu√Å‚Äûo
+	float getTime() { return processingTime; }
+
+	//m√àtodo que imprime o vetor solu√Å‚Äûo codificada
+	vector<int> getCode() { return codifiedSolution; }
+
+	//m√àtodo que redefine nÀômero de bins de uma solu√Å‚Äûo
+	void resetNumberBins(int n) { numberOfBins = n; }
 
 };
 
 
-/*******************************CLASSE EVOLUTIONARY ALGORITHM E SEUS M…TODOS *********************************/
+/*******************************CLASSE EVOLUTIONARY ALGORITHM E SEUS M‚Ä¶TODOS *********************************/
 class EA {
 private:
 
-	int nItems;						//atributo que guarda o n˙mero de elementos da inst‚ncia
-	int binCapacity;				//atributo que guarda a capacidade dos bins da inst‚ncia
+	int nItems;						//atributo que guarda o nÀômero de elementos da inst‚Äöncia
+	int binCapacity;				//atributo que guarda a capacidade dos bins da inst‚Äöncia
 	string name;					//nome da instancia, sem extensao do arquivo de entrada
-	vector<Solution> solutionPath;	//vetor de melhores soluÁıes encontradas (pensar se faz sentido)
+	vector<Solution> solutionPath;	//vetor de solu√Åƒ±es pais
 	double bestFitness;				//atributo que guarda o melhor valor da Fitness Function encontrado
-	unsigned bestSolutionPosition;		//atributo que guarda a posiÁ„o da soluÁ„o final (melhor encontrada)
+	unsigned bestSolutionPosition;	//atributo que guarda a posi√Å‚Äûo da solu√Å‚Äûo final (melhor encontrada)
+	int relaxedLB;					//vari¬∑vel que guarda o LB do problema, dado pelo BPP com xij real
 
 public:
 
-	//construtor da classe EA que usa uma soluÁ„o inicial, a partir da qual executa a heurÌstica (usado nos testes)
+	//construtor da classe EA que usa uma solu√ß√£o inicial, a partir da qual executa a heur√åstica (usado nos testes)
 	EA(Solution &s0);
 
-	//construtor que usa a inst‚ncia - primeiro acha soluÁ„o inicial pela construtiva depois executa a heurÌstica
+	//construtor que usa a inst√¢ncia - primeiro acha solu√ß√£o inicial pela construtiva depois executa a heur√åstica
 	EA(Instances &inst);
 
-	//mÈtodo que encontra soluÁ„o inicial aplicando o First Fit modificado (categorias ordenadas)
+	//m√àtodo que encontra solu√ß√£o inicial aplicando o First Fit modificado (categorias ordenadas)
 	void generate_s0(Instances &inst);
 
-	//mÈtodo que aplica o algoritmo evolucion·rio
+	//m√àtodo que aplica o algoritmo evolucion√°rio
 	void applyEA();
 
-	//mÈtodo que gera filho
-	void generateSun(vector<Solution> &offspring, CRandomMersenne &ran);
+	//m√àtodo que gera filho
+	void generateSun(vector<Solution> &offspring, CRandomMersenne &ran, Timer t);
 
-	//mÈtodo que imprime evoluÁ„o da heurÌstica
+	//m√àtodo que imprime evolu√ß√£o da heur√≠stica
 	void printEvolution();
 
-	//mÈtodos que retornam os valores dos atributos
+	//m√àtodos que retornam os valores dos atributos
 	vector<Solution> getSolutionPath(){ return solutionPath; }
 	double getFinalFitness() { return bestFitness; }
 	int getProblemSize() { return nItems; }
 	int getBinCap() { return binCapacity; }
 	string getInstanceName() { return name; }
 	unsigned getFinalSPosition() { return bestSolutionPosition; }
+	int getLB1() { return relaxedLB; }
 
 };
